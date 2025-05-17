27a5ff1cd1b8bfce68fd634e8c9ed6cb1df25fc7
Administrador <administrador@MacBookPro-gonzalocastillo.local>
2025-05-17 15:28:29 -0600

    feat: basic json web token

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: any) {
+    this.$on("beforeExit", async () => {
+      await app.close();
+    });
+  }
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}

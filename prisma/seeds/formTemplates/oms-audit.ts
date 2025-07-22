319ee5fe6009aea0b14de81ca9cdc1e028c80a2d
Administrador <administrador@MacBookPro-gonzalocastillo.local>
2025-07-21 23:42:11 -0600

    Change api version on stripe

--- prisma/seeds/formTemplates/oms-audit.ts
+++ prisma/seeds/formTemplates/oms-audit.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../@prisma/client/client';
+import { PrismaClient } from '@prisma/client';
 import {formTemplateConstants} from '../contants';
 
 const prisma = new PrismaClient();

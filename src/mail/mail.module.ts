0c511a6063f6ab380bd8e7d6b775a93dd6ede3c6
Roger Torres <torresroger445@gmail.com>
2025-09-14 18:47:17 -0600

    fix: :fire: update mailer config

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -2,28 +2,24 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 
 @Module({
   imports: [
     ConfigModule,
-    MailerModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (config: ConfigService) => ({
-        transport: {
-          host: config.get('SMTP_HOST'),
-          port: config.get('SMTP_PORT'),
-          secure: config.get('SMTP_SECURE') === 'true',
-          auth: {
-            user: config.get('SMTP_USER'),
-            pass: config.get('SMTP_PASS'),
-          },
+    MailerModule.forRoot({
+      transport: process.env.MAILER_URL ?? '',
+      defaults: {
+        from: '"hello" <hello@escalaterapia.com>',
+      },
+      template: {
+        dir: __dirname + '/srs/mail/templates',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
         },
-        defaults: {
-          from: `"Escala Terapia" <${config.get('SMTP_FROM')}>`,
-        },
-      }),
-      inject: [ConfigService],
+      },
     }),
   ],
   providers: [MailService],
